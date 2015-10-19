463ad59e2445f69ae28e02709d9d6bcd63a9fb73
Paweł Kłeczek <pkleczek@gmail.com>
2015-10-19 20:52:44 +0200

    Update core movement logic - Add forward method - Update unit tests and invalid destructuring

--- src/lant/core.clj
+++ src/lant/core.clj
@@ -1,15 +1,20 @@
 (ns lant.core)
 
+;positions with following position for left and right side of an ant
 (def dirs {:north [:west :east]
            :east [:north :south] 
            :south [:east :west] 
            :west [:south :north]})
 
-(def dir-vector {:north [0 1] :east [1 0] :south [0 -1] :west [-1 0]})
+(def dir-vector {:north [0 -1] :east [1 0] :south [0 1] :west [-1 0]})
 
 (defn make-ant 
   ([] {:pos [0 0] :dir :north})
   ([x y dir] {:pos [x y] :dir dir}))
+
+(defn- rotate
+  [{[x y] :pos dir :dir} idx]
+  (make-ant x y ((dir dirs) idx)))
 
 (defn rotate-left
   [ant] (rotate ant 0))
@@ -17,6 +22,8 @@
 (defn rotate-right
   [ant] (rotate ant 1))
 
-(defn- rotate
-  [{{x :x y :y} :pos dir :dir} idx]
-  (make-ant x y ((dir dirs) idx)))
+(defn forward
+  [{[x y] :pos dir :dir}]
+  (let [movement (dir-vector dir)
+        new-pos (mapv + [x y] movement)]
+    (make-ant (new-pos 0) (new-pos 1) dir)))
--- test/lant/core_test.clj
+++ test/lant/core_test.clj
@@ -3,12 +3,15 @@
             [lant.core :refer :all]))
 
 (deftest ant-movement
-  (testing "Ant is set :north by default"
-    (is (= :north (:dir (make-ant)))))
+  (testing "Ant is set :north by default in (0,0)"
+    (is (= {:pos [0 0] :dir :north} (make-ant))))
 
   (testing "Ant can rotate left"
-    (is (= :west (:dir (rotate-left (make-ant))))))
+    (is (= (make-ant 0 0 :west) (rotate-left (make-ant)))))
 
   (testing "Ant can rotate right"
-    (is (= :east (:dir (rotate-right (make-ant))))))
+    (is (= (make-ant 0 0 :east) (rotate-right (make-ant)))))
+
+  (testing "Ant can go forward"
+    (is (= (make-ant 3 4 :west)(forward (make-ant 4 4 :west)))))
 )
